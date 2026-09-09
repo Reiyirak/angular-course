34aead268432766ea2d259de4d3806ae9e24f4ee
Reiyirak <pdsmej@gmail.com>
2026-09-09 13:38:31 -0600

    [feature] Create environments and path alias
    The environments values were also added to the side-menu-header
    component

--- 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
 
 @Component({
   imports: [],
   selector: 'gifs-side-menu-header',
   templateUrl: './side-menu-header.html',
 })
-export class SideMenuHeader { }
+export class SideMenuHeader {
+  envs = environment
+}
