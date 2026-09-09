6de2c354e3d6292fcf1da12ebbcd12bc48331155
Reiyirak <pdsmej@gmail.com>
2026-09-09 13:16:46 -0600

    [Feature] Rotuer links from child components

--- 03-gifs-app/src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   selector: 'gifs-side-menu-options',
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs Populares',
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      route: '/dashboard/search',
+      subLabel: 'Buscar gifs',
+    }
+  ];
+}
