5fb8e850b74b0ce5aa88e464cab51cdb48362170
Reiyirak <pdsmej@gmail.com>
2026-09-10 17:53:28 -0600

    [Feature] Display results from http request
    Now the gifs are displayed on the trending page and search page.

--- 03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -4,12 +4,14 @@ import { GiphyResponse } from "../interfaces/giphy.interfaces";
 import { environment } from "@environments/environment";
 import { Gif } from "../interfaces/gif.interface";
 import { GifMapper } from "../mapper/gif.mapper";
+import { map } from "rxjs";
 
 @Injectable({ providedIn: 'root' })
 export class GifService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([])
+  trendingGifsLoading = signal(true);
 
   constructor() {
     this.loadTrendingGifs();
@@ -25,7 +27,28 @@ export class GifService {
     })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemToGifArray(resp.data);
+        this.trendingGifs.set(gifs);
+        this.trendingGifsLoading.set(false);
         console.log({ gifs });
       });
+  }
+
+  searchGifs(query: string) {
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        api_key: environment.giphyApiKey,
+        limit: 100,
+        rating: 'r',
+        q: query,
+      }
+    })
+      .pipe(
+        map(({ data }) => data),
+        map((items) => GifMapper.mapGiphyItemToGifArray(items)),
+      );
+    // .subscribe((resp) => {
+    //   const gifs = GifMapper.mapGiphyItemToGifArray(resp.data);
+    //   console.log({ search: gifs });
+    // });
   }
 }
