3f47c390a5e8afecf496d81a3f978e5e639e98e2
Reiyirak <pdsmej@gmail.com>
2026-09-10 11:55:10 -0600

    [Feature] Add giphy interface and env variables
    Also injected the Http client to a new giphy serivice.
    The client was procived in the app config.

--- 03-gifs-app/src/environments/environment.ts
+++ 03-gifs-app/src/environments/environment.ts
@@ -5,6 +5,8 @@ export const environment = {
   companySlogan: 'Maneja tus gifs',
 
   // ApiKeys
+  giphyApiKey: 'tiARNkZVXqWhnIFTh4kaFTwdALXrDhBZ',
+  giphyUrl: 'https://api.giphy.com/v1',
 
   // URLS
 };
